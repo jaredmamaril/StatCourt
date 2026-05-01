e5babdf17df4d2b906522d700b41a619c2d74506
mattjaredmamaril <tytjared@gmail.com>
2026-05-01 13:33:26 -0500

    Moved all files under root instead of duplicate subfolder

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+
+export default function Home() {
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-between p-24">
+      <h1 className="text-4xl font-bold">Welcome to Stat Court</h1>
+    </div>
+  
+  );
+}
