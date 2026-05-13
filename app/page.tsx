95f6326d0ac73f6f1d160658317932cf776f49e2
mattjaredmamaril <tytjared@gmail.com>
2026-05-13 12:16:44 -0500

    Refactor Home component layout and improve header styling for basis design

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,18 @@
+import Image from "next/image";
+
 export default function Home() {
   return (
     <main className="min-h-screen bg-slate-50 text-white">
-      <header className="flex items-center justify-between px-8 py-5">
-        <div className="flex-items center gap-3">
-          <Image>src=</Image>
+      <header className="flex items-center justify-between px-4 py-5">
+        <div className="flex items-center gap-4">
+          <Image
+            src="/capy1.png"
+            alt="temp"
+            width={60}
+            height={60}
+            className="rounded-md"
+          />
+          <span className="text-xl font-bold bg-black">StatCourt</span>
         </div>
       </header>
     </main>
